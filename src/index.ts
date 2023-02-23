b14676ad421f43d7b1f1377bcb424dee2c0809d9
Jeremy Cross <remycross1019@gmail.com>
2023-02-23 13:17:57 -0500

    Checking why things aren't working

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,24 @@ const jobDaily = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
   }
 });
 
-const jobWeekly = new CronJob(env.BACKUP_CRON_SCHEDULE_WEEKLY, async () => {
-  try {
-    await backup('weekly');
-  } catch (error) {
-    console.error("Error while running backup: ", error)
-  }
-});
+// const jobWeekly = new CronJob(env.BACKUP_CRON_SCHEDULE_WEEKLY, async () => {
+//   try {
+//     await backup('weekly');
+//   } catch (error) {
+//     console.error("Error while running backup: ", error)
+//   }
+// });
 
-const jobMonthly = new CronJob(env.BACKUP_CRON_SCHEDULE_MONTHLY, async () => {
-  try {
-    await backup('monthly');
-  } catch (error) {
-    console.error("Error while running backup: ", error)
-  }
-});
+// const jobMonthly = new CronJob(env.BACKUP_CRON_SCHEDULE_MONTHLY, async () => {
+//   try {
+//     await backup('monthly');
+//   } catch (error) {
+//     console.error("Error while running backup: ", error)
+//   }
+// });
 
 jobDaily.start();
-jobWeekly.start();
-jobMonthly.start();
+// jobWeekly.start();
+// jobMonthly.start();
 
 console.log("Backup cron scheduled...")
